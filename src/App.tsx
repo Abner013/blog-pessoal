13412c3b78d39ce83197033d4bce951b1370e118
unknown <abner_borges5712@outlook.com>
2022-01-25 12:35:28 -0300

    react.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import {BrowserRouter as Router, Route , Switch } from 'react-router-dom'
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
 
 function App() {
   return (
-    <>
+    <div>
+    <Router>
       <Navbar />
-      <Home />
+      <Switch >
+        <div style={{minHeight: '100vh'}}>
+          
+          <Route path='/login'>
+            <Login />
+          </Route>
+          
+          <Route exact path="/home">
+            <Home />
+          </Route>
+        </div>
+      </Switch>
       <Footer />
-    </>
-  );
+    </Router>
+    </div>
+  )
 }
 
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,26 +4,26 @@ import './Home.css'; //componete esta importando o css
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "black" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Bem vindo(a)!</Typography>
-                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Nos conte o que está pensado!</Typography>
+                        <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Bem vindo(a)!</Typography>
+                        <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Nos conte o que está pensado!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "black", color: "white" }}>Postagens</Button>
+                        <Button variant="outlined" className="botao">Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <img src="https://cdn.pixabay.com/photo/2019/08/06/22/48/artificial-intelligence-4389372_960_720.jpg" alt="center" width="700px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "whiter" }}>
+                <Grid xs={12} className="postagens">
                 </Grid>
             </Grid>
         </>
-    );
+    )
 }
 
 export default Home;
